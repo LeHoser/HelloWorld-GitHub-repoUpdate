ec8284d1ca7933e2d160837d995a64d9fd567c8c
LeHoser <nathan.lloyd.7003@gmail.com>
2022-09-14 13:37:17 -0300

    Added another WriteLine

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ namespace HelloWorld_GitHub_repoUpdate
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+            Console.WriteLine("This is an update");
             Console.ReadKey(true);
         }
     }
